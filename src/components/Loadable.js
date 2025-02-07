f823a00322abd1257d44c02c058ce8dbea34e924
mwinamijr <mwinamijr@gmail.com>
2025-02-07 22:38:48 +0300

    created components for the layot and pages

---
+++ src/components/Loadable.js
@@ -0,0 +1,29 @@
+import { Suspense } from "react";
+import LinearProgress from "@mui/material/LinearProgress";
+import { styled } from "@mui/material/styles";
+
+// Styles for the loader
+const LoaderWrapper = styled("div")({
+  position: "fixed",
+  top: 0,
+  left: 0,
+  zIndex: 1301,
+  width: "100%",
+});
+
+// Loader Component
+const Loader = () => (
+  <LoaderWrapper>
+    <LinearProgress color="primary" />
+  </LoaderWrapper>
+);
+
+// Loadable Component combining Suspense and Loader
+const Loadable = (Component) => (props) =>
+  (
+    <Suspense fallback={<Loader />}>
+      <Component {...props} />
+    </Suspense>
+  );
+
+export default Loadable;
